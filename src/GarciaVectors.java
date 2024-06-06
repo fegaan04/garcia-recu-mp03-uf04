59bbd2bd9c3866cbbc8e3b5cb85bd4461b6a6b49
fegaan04 <ferran0031@gmail.com>
2024-06-06 16:19:53 +0200

    ultim commit

--- src/GarciaVectors.java
+++ src/GarciaVectors.java
@@ -4,12 +4,9 @@ public class GarciaVectors {
 
     int midaDelVector = 5;
     int[] vOrigen = new int[midaDelVector];
+
     public static double garciaAleatori() {
         return Math.random()*(100-(1))+(1);
     }
-    public static double garciaaleatori() {
-        return Math.random() * (ENTER_MAX - ENTER_MIN);
-    }
-
 
 }
